c548a6907626c2dff61cdf909f185e2726785a43
Yury Kovaliov <yurykavaliou86@gmail.com>
2022-01-12 13:14:05 +0300

    feat: added an object creation class and a data editing area

--- task08_JS_Promise/main.js
+++ task08_JS_Promise/main.js
@@ -5,7 +5,8 @@ const arrCardItem1 = document.querySelectorAll('.card-1 span'),
       arrCardItem3 = document.querySelectorAll('.card-3 span'),
       arrStatus = document.querySelectorAll('.status');
 
-const inputTask = document.querySelector('.num'),
+const inputs = document.forms.form,
+      inputTask = document.querySelector('.num'),
       inputDate = document.querySelector('.card-5 .date'),
       inputTime = document.querySelector('.card-5 .time'),
       inputName = document.querySelector('.card-5 .name'),
@@ -21,32 +22,70 @@ const arrUrl = [
 
 const arrJson = [];
 
+class TodoItem {
+  constructor(date, time, name, important, note) {
+    this.date = date.value,
+    this.time = time.value,
+    this.name = name.value,
+    this.important = important.checked,
+    this.note = note.value
+  }
+
+  isValid() {
+    let valid = [];
+    for(let key in this) {
+      if(this[key] === '') {
+        inputs[key].classList.add('invalid');
+        valid.push(false);
+      } else {
+        inputs[key].classList.remove('invalid');
+        this.date = this.date.split('-').reverse().join('-');
+        valid.push(true);
+      }
+    }
+    return valid.every(item => item === true);
+  }
+
+  async submitNewTodoItem(e) {
+    e.preventDefault();
+    if(this.isValid()) {
+      const response = await fetch(arrUrl[inputTask.value - 1], {
+        method: 'PUT',
+        body: JSON.stringify(this),
+        headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        'Allow': 'PUT',
+        'Access-Control-Allow-Methods': 'PUT',
+        }
+      });
+    }
+  }
+}
+
 function getData(url) {
-  const responses = [];
-
-  url.forEach(async u => {
+  url.forEach(async (u, index) => {
     const res = await fetch(u);
-    responses.push(res);
-
     const json = await res.json();
     arrJson.push(json);
-
-    arrStatus.forEach((item, index) => {
-      item.innerText = responses[index].statusText;
-    });
+    arrStatus[index].innerText = res.statusText;
   }); 
 }
-      
-function addDataToPage(arr, obj) {
-  arr.forEach(item => {
-    item.innerText = obj[item.className];
+
+function showTodoItem(arrElements, jsonObj) {
+  arrElements.forEach(item => {
+    item.innerText = jsonObj[item.className];
   })
 }
 
 getData(arrUrl);
-      
-setTimeout(() => addDataToPage(arrCardItem1, arrJson[0]), 1000);
-setTimeout(() => addDataToPage(arrCardItem2, arrJson[1]), 2000);
-setTimeout(() => addDataToPage(arrCardItem3, arrJson[2]), 3000);
+
+setTimeout(() => showTodoItem(arrCardItem1, arrJson[0]), 1000);
+setTimeout(() => showTodoItem(arrCardItem2, arrJson[1]), 2000);
+setTimeout(() => showTodoItem(arrCardItem3, arrJson[2]), 3000);
 
 // setInterval(() => getData(arrUrl), 3000);
+
+submit.addEventListener('click', (e) => {
+  const todoItem = new TodoItem(inputDate, inputTime, inputName, inputImportant, inputNote);
+  todoItem.submitNewTodoItem(e)
+})
